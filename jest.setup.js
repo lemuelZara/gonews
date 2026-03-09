ad6557ff80cbeb69ab00ff17240b3a141d1f9151
Lemuel <lemuel.czara@gmail.com>
2026-03-09 23:42:27 +0000

    linter

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,8 @@
 import { loadEnvConfig } from "@next/env";
 
-export default async () => {
+const setup = async () => {
   const projectDir = process.cwd();
   loadEnvConfig(projectDir);
 };
+
+export default setup;
